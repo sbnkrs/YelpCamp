4949687405fe8bbb708914eb238f49037380a384
sbnkrs <shabankrasniqi.it@gmail.com>
2023-11-08 12:20:04 +0100

    seeds/index.js update

--- seeds/index.js
+++ seeds/index.js
@@ -26,7 +26,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
-            author: '651d2aa4772129ffe9b91499',
+            author: '654b6e6850e06f80a0a7e70f',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'This is a campground of YelpCamp',
